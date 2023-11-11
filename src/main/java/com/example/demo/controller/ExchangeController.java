e7e7ef8cc2858983dd07001b3433aada3cbd351f
yoyo <hsjejxys@gmail.com>
2023-11-11 22:13:39 +0900

    リクエストマッピングの調整

--- src/main/java/com/example/demo/controller/ExchangeController.java
+++ src/main/java/com/example/demo/controller/ExchangeController.java
@@ -16,7 +16,7 @@ import com.example.demo.repositories.Train1;
 @Controller
 public class ExchangeController {
 	
-	@RequestMapping("https://exchangetrainforme.onrender.com/")
+	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		//遷移するページを追加
 		mav.setViewName("home");
